ea42924dd7b7143d779eaed5a9bbb3ffea781232
Mahesh <mmkdissa@gmail.com>
2018-12-05 13:37:06 +0530

    added test cloud function

--- cloud/functions/hello.js
+++ cloud/functions/hello.js
@@ -1,3 +1,16 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('world');
 });
+
+Parse.Cloud.define('createBizAccount',function(req,res){
+  const stripe = require('stripe')('sk_test_CqDHZlR0lvt66Jo9gKFundJ4');
+  stripe.accounts.create({
+    type: 'custom',
+    country: 'US',
+    email: 'bob@example.com'
+  }, function (err, account) {
+     console.log(err);
+     console.log(account);     
+     res.success(account);
+  });
+});
