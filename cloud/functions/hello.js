48a6df430827550aa54ec1a5b65b7987668f7d55
Mahesh <mmkdissa@gmail.com>
2018-12-05 14:08:06 +0530

    updated cloud function to create account with provided email

--- cloud/functions/hello.js
+++ cloud/functions/hello.js
@@ -4,13 +4,19 @@ Parse.Cloud.define('hello', function(req, res) {
 
 Parse.Cloud.define('createBizAccount',function(req,res){
   const stripe = require('stripe')('sk_test_CqDHZlR0lvt66Jo9gKFundJ4');
+  var bizEmail = request.params.email;
   stripe.accounts.create({
     type: 'custom',
     country: 'US',
-    email: 'bob@example.com'
+    email: bizEmail
   }, function (err, account) {
      console.log(err);
-     console.log(account);     
-     res.success(account);
+     console.log(account); 
+     if(err != null){
+      res.success(account);
+     }else{
+       res.error(err);
+     }
+    
   });
 });
