bd44415754c8d743ed6c771c0e4c2b83645fb326
mmkdissa <mmkdissa@gmail.com>
2019-03-04 15:52:11 +0530

    updated cloud function beforeSave RestaurantMenuItemGroup

--- cloud/main.js
+++ cloud/main.js
@@ -1,12 +1,9 @@
 require("./functions/hello");
 
-Parse.Cloud.beforeSave("RestaurantMenuItemGroup", function (request, response) {
+Parse.Cloud.beforeSave("RestaurantMenuItemGroup", function (request) {
 
     //check if this is a new or existing item
-    if (!request.object.isNew()) {
-        response.success();
-    } else {
-
+    if (request.object.isNew()) {
         var restaurantMenuItemGroup = Parse.Object.extend("RestaurantMenuItemGroup");
         var query = new Parse.Query(restaurantMenuItemGroup);
         query.select("code");
@@ -16,11 +13,10 @@ Parse.Cloud.beforeSave("RestaurantMenuItemGroup", function (request, response) {
             function (lastItemGroup) {
                 //increase the code by 1
                 var newId = lastItemGroup.get("code") + 1;
-                request.object.set("code", newId);
-                response.success();
+                request.object.set("code", newId);               
             },
             function (error) {
-                response.error(error);
+                throw 'error creating automatic code'; 
             }
         );
     }
