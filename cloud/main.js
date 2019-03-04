a5ecbe55a768e61f47042637f28c4be2d19ee3d7
mmkdissa <mmkdissa@gmail.com>
2019-03-04 16:18:21 +0530

    updated cloud function
    Signed-off-by: mmkdissa <mmkdissa@gmail.com>

--- cloud/main.js
+++ cloud/main.js
@@ -9,7 +9,7 @@ Parse.Cloud.beforeSave("RestaurantMenuItemGroup", function (request) {
         query.select("code");
         query.descending("code");
 
-        let lastItemGroup =  query.first();
+        var lastItemGroup = await query.first();
         var newId = lastItemGroup.get("code") + 1;
         request.object.set("code", newId);  
 
