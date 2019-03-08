aa3b3b87eba5f70930e82db66b51e86e3c118a2b
mmkdissa <mmkdissa@gmail.com>
2019-03-08 10:57:25 +0530

    set config to dev

--- public/assets/js/config.js
+++ public/assets/js/config.js
@@ -1,6 +1,15 @@
-function Config(){
-	
+function Config() {
+
 }
 
-Config.PARSE_SERVER_URL = "https://peppequeue.herokuapp.com/parse";
-Config.PARSE_APP_ID = "B1308B7F93AA08C8764712FF391ED073CBDDDEC1C834F95BE03C61916C5CCB18";
+/**Prod */
+//Config.PARSE_SERVER_URL = 'https://peppequeue.herokuapp.com/parse';
+//Config.PARSE_APP_ID = 'B1308B7F93AA08C8764712FF391ED073CBDDDEC1C834F95BE03C61916C5CCB18';
+
+/**Dev */
+Config.PARSE_SERVER_URL = 'https://dev-peppequeue.herokuapp.com/parse';
+Config.PARSE_APP_ID = 'E017VL2U96F5YU489U2K';
+
+/*Local*/
+//Config.PARSE_SERVER_URL = 'http://localhost:1337/parse';
+//Config.PARSE_APP_ID = 'JSJDSD737434JCJAJK893943';
